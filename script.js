b8ad219db2ab4225b94da832ce1227e83f3470a9
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2024-01-21 18:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+  let indAt=-1,indDot=-1;
+  for(let i=0;i<str.length;i++){
+	  if(str[i]==='@') indAt=i;
+	  if(str[i]==='.' && indDot===-1) indDot=i;
+  }
+  if(indAt===-1 || indAt===0 || indAt>=indDot) return false;
+  return true;
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
-alert(validEmail(str));
+alert(validEmail(str)); 
